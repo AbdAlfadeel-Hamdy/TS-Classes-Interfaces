580c35daf11f2d01003d068c9b684d7477a481ae
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-09 19:59:09 +0200

    classes

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
 class Department {
-  name: string;
-  constructor(name: string) {
-    this.name = name;
+  // private name: string;
+  private employees: string[] = [];
+  constructor(private readonly id: string, private name: string) {
+    // this.name = name;
   }
   describe(this: Department): void {
-    console.log("Department: " + this.name);
+    console.log(`Department(${this.id}): ` + this.name);
+    // this.id = "p2"; // Invalid code because its readonly
+  }
+  addEmployee(employee: string): void {
+    this.employees.push(employee);
+  }
+  printEmployeesInformation(): void {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
+const accounting = new Department("d1", "Accounting");
 accounting.describe();
-console.log(accounting);
+accounting.addEmployee("Max");
+accounting.addEmployee("Stephen");
+// accounting.employees[2] = "Hossam"; Not the best practice
+accounting.printEmployeesInformation();
+// console.log(accounting);
 
 const accountingCopy = {
   name: "Gonzalo",
@@ -18,4 +31,4 @@ const accountingCopy = {
 };
 
 accountingCopy.describe.bind(accounting);
-accountingCopy.describe();
+// accountingCopy.describe(); // It has to be of type Department to avoid errors
