9f64706eaac7323ab415ce2f3267ec430dfbbc88
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-09 19:42:22 +0200

    this argument

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-// Code goes here!
+class Department {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  describe(this: Department): void {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department("Accounting");
+accounting.describe();
+console.log(accounting);
+
+const accountingCopy = {
+  name: "Gonzalo",
+  describe: accounting.describe,
+};
+
+accountingCopy.describe.bind(accounting);
+accountingCopy.describe();
